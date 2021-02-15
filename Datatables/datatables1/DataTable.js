985ea5f8138d9e821f7bb2dd84c1cb5828f3c6fc
alejandrosergio <alejosergio3000@gmail.com>
2021-02-14 21:02:28 -0500

    datatables-modal

--- Datatables/datatables1/DataTable.js
+++ Datatables/datatables1/DataTable.js
@@ -1,5 +1,44 @@
+/* MODAL */
+
+let cerrar = document.querySelectorAll(".close_modal")[0];
+let abrir = document.querySelectorAll(".llamar_modal")[0];
+let modal = document.querySelectorAll(".modal_mod")[0];
+let modalC = document.querySelectorAll(".main-modal_container")[0];
+
+abrir.addEventListener("click", function(e){
+    e.preventDefault();
+    modalC.style.opacity = "1";
+    modalC.style.visibility = "visible";
+    modal.classList.toggle("modal-close");
+});
+
+cerrar.addEventListener("click",function(){
+    modal.classList.toggle("modal-close");
+
+    setTimeout(function(){
+        modalC.style.opacity = "0";
+        modalC.style.visibility = "hidden";
+    },700)
+});
+
+window.addEventListener("click",function(e){
+    if (e.target == modalC) {
+        modal.classList.toggle("modal-close");
+
+    setTimeout(function(){
+        modalC.style.opacity = "0";
+        modalC.style.visibility = "hidden";
+    },600)
+    }
+});
+
+/* DATATABLE */
+
 $(document).ready(function() {    
-    $('#example').DataTable({        
+    $('#example').DataTable({ 
+		"paging":   true,
+        "ordering": true,
+        "info":     false,
         language: {
                 "lengthMenu": "Mostrar _MENU_ registros",
                 "zeroRecords": "No se encontraron resultados",
@@ -12,8 +51,8 @@ $(document).ready(function() {
                     "sLast":"Último",
                     "sNext":"Siguiente",
                     "sPrevious": "Anterior"
-			     },
-			     "sProcessing":"Procesando...",
+				},
+				"sProcessing":"Procesando...",
             },
         //para usar los botones   
         responsive: "true",
